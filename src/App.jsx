ab81dd7f9be88dc076b3328cd911d0b91a60c22a
Thomas <thomas@craftacademy.se>
2020-04-19 06:00:21 +0200

    cleans up code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+
+const App = () => {
+  const [promoText, setPromoText] = useState({})
+  const 
+
+  return (
+    <>
+      <h1>Craft Academy Nomads</h1>
+    </>
+  )
+}
+
+export default App
